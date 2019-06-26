df094497a924d3c03a73bdd957c3f30160b97394
CAH <hamiltonlab@uidaho.edu>
2019-06-26 16:16:18 -0700

    updates

---
+++ _site/assets/js/search-data.js
@@ -0,0 +1,126 @@
+var store = [{
+        "title": "The new PNW herbarium portal",
+        "excerpt":"www.pnwherbaria.org.The Consortium of Pacific Northwest Herbaria data portal has recently undergone a major update with substantially increased data content. If herbarium specimen data is of interest to you, I encourage you to take a look. The Consortium represents herbaria in the U.S. states of Alaska, Idaho, Montana, Oregon, and Washington,...","categories": ["articles"],
+        "tags": ["Consortium of PNW herbaria","NSF","DBI","specimen data"],
+        "url": "http://localhost:4000/articles/pnwherbaria/"
+      },{
+        "title": "NSF DDIG!",
+        "excerpt":"Systematics of Bartsia.Congratulations to Simon Uribe-Convers on his successful NSF Doctoral Dissertation Improvement Grant (DDIG) proposal! “Investigating the accumulation of evidence for speciation: species delimitation in a rapid and recent radiation.” This study seeks to understand how the evidence for speciation has accumulated over time in the mostly South American...","categories": ["articles"],
+        "tags": ["Simon Uribe-Convers","NSF","DDIG","Bartsia"],
+        "url": "http://localhost:4000/articles/Simon-DDIG/"
+      },{
+        "title": "Teacher of the Year!",
+        "excerpt":"\t\tTeaching Dendrology.I am honored to be the 2012 College of Natural Resources Teacher of the Year! The best part about this award is that the nominations and votes come from the students. That’s pretty cool - thanks! ","categories": ["articles"],
+        "tags": ["Dave Tank","teaching award"],
+        "url": "http://localhost:4000/articles/teaching-award/"
+      },{
+        "title": "Plants are Cool, Too!",
+        "excerpt":"Check out our lab on the second episode of “Plants are Cool, Too!” with host Dr. Chris Martine of Bucknell University. We had a lot of fun making this and hope you enjoy learning about the 15 million year old “Fossilized Forests” of northern Idaho. ","categories": ["articles"],
+        "tags": ["Plants are cool","too!","fossilized forests","Clarkia","Miocene","Bill Rember"],
+        "url": "http://localhost:4000/articles/plants-are-cool-too/"
+      },{
+        "title": "NSF CAREER proposal funded!",
+        "excerpt":"\t\t\tSystematics of Castilleja.I am super excited to announce that my proposal to the NSF’s Faculty Early Career Development (CAREER) program for for integrated research and teaching on systematics of Castilleja was funded! Check out the UIdaho news release here. ","categories": ["articles"],
+        "tags": ["NSF","DEB","Phylogenetic Systematics","grants"],
+        "url": "http://localhost:4000/articles/NSF-career/"
+      },{
+        "title": "Congruification paper published.",
+        "excerpt":"Eastman, Harmon, and Tank. 2013. MEE.Our method for time scaling large phylogenetic trees has been published in Methods in Ecology and Evolution. This is also the code behind the DateLife project - a Phylotastic product. Eastman, JM, LJ Harmon, and DC Tank. 2013. Congruification: support for time scaling large phylogenetic...","categories": ["articles"],
+        "tags": ["congruification","time tree","megaphylogeny","MEE","Jon Eastman","Luke Harmon"],
+        "url": "http://localhost:4000/articles/congruification/"
+      },{
+        "title": "Campanulid biogeography paper published.",
+        "excerpt":"Beaulieu, Tank, and Donoghue. 2013. BMC Evolution.A Southern Hemisphere origin for campanulid angiosperms, with traces of the break-up of Gondwana with Jeremy Beaulieu and Michael Donoghue was published this week in BMC Evolutionary Biology. This paper provides a great discussion on discovering the Gondwanan biogeographic history of plant lineages and...","categories": ["articles"],
+        "tags": ["campanulid","biogeography","Gondwana","BMC Evolutionary Biology","Jeremy Beaulieu","Michael Donoghue"],
+        "url": "http://localhost:4000/articles/campanulid-biogeography/"
+      },{
+        "title": "Student Grants!",
+        "excerpt":"Graduate students Hannah Marx &amp; Diego Morales-Briones.Congratulations to graduate students Diego Morales Briones and Hannah Marx! Both received multiple graduate student awards this spring. Hannah was awarded a Society for the Study of Evolution Rosemary Grant Award for Graduate Student Research and a Society of Systematic Biologists Graduate Student Research...","categories": ["articles"],
+        "tags": ["graduate student awards","Hannah Marx","Diego Morales-Briones","SSE","SSB","BSA","ASPT"],
+        "url": "http://localhost:4000/articles/student-grants/"
+      },{
+        "title": "Nature paper!",
+        "excerpt":"Zanne et al. 2014. Nature.The first paper from the NESCent tempo &amp; mode of plant trait evolution working group on the evolution of cold tolerance in angiosperms was published online today in Nature. All of our data is deposited in Dryad - available here. Here is a compilation of news...","categories": ["articles"],
+        "tags": ["nescent","tempo and mode","cold tolerance","Nature"],
+        "url": "http://localhost:4000/articles/nature/"
+      },{
+        "title": "Plant tree on OneZoom!",
+        "excerpt":"Explore the Seed Plant Tree of Life using the OneZoom tree viewer! This phylogeny of 30,000+ seed plants is from our recent paper with branches colored by woody/herbaceous habit. Zanne, AE, DC Tank, WK Cornwell, JM Eastman et al. 2014. Three keys to the radiation of angiosperms into freezing environments....","categories": ["articles"],
+        "tags": ["OneZoom","spermatophyta","cold tolerance","nature"],
+        "url": "http://localhost:4000/articles/onezoom-plants/"
+      },{
+        "title": "Welcome, Maribeth!",
+        "excerpt":"\t\t\tMaribeth Latvis&lt;/i&gt;.The Tank Lab is very happy to welcome Dr. Maribeth Latvis to the University of Idaho.  Maribeth comes to us from the Soltis lab at the University of Florida where she finished her PhD working on the systematics and evolution of the South American clade of Agalinis (Orobanchaceae). ","categories": ["articles"],
+        "tags": ["Maribeth Latvis","postdoc","tank lab"],
+        "url": "http://localhost:4000/articles/welcome-maribeth/"
+      },{
+        "title": "APPS paper published",
+        "excerpt":"Uribe-Convers et al. 2014. APPS.Simon’s paper using long PCR and Illumina sequencing to generate complete plastomes in angiosperms was published in the January issue of APPS. This also includes a set of universal PCR primers for angiosperms. Check out the EurekAlert! and ScienceDaily press releases. Uribe-Convers, S, JR Duke, MJ...","categories": ["articles"],
+        "tags": ["Simon Uribe-Convers","plastome","HTS","APPS"],
+        "url": "http://localhost:4000/articles/apps-paper/"
+      },{
+        "title": "MinuteEarth on trees & freezing",
+        "excerpt":"Peter Reich - coauthor on the recent Nature paper - worked with the MinuteEarth team to put together a video on how trees survive winter. Very cool to see how this draws from our paper! ","categories": ["articles"],
+        "tags": ["Minute Earth","tempo and mode","cold tolerance","nature"],
+        "url": "http://localhost:4000/articles/minute-earth/"
+      },{
+        "title": "Functional distinctiveness in plants",
+        "excerpt":"Cornwell et al. 2014. J Ecol.The 2nd paper from the NESCent tempo &amp; mode of plant trait evolution working group on functional trait diversity in Angiosperms came out today in a special issue of the Journal of Ecology. Cornwell, WC et al. Functional distinctiveness of major plant lineages. 2014. Journal...","categories": ["articles"],
+        "tags": ["nescent","tempo and mode","traits","journal of ecology"],
+        "url": "http://localhost:4000/articles/functional-distinctiveness/"
+      },{
+        "title": "Congratulations, Diego!",
+        "excerpt":"Diego &amp; Espeletia.Congratulations to Diego Morales-Briones for his successful International Association for Plant Taxonomy grant proposal! Diego’s proposal was entitled “A comprehensive chloroplast phylogeny of the genus Lachemilla (Rosaceae) and relatives using a novel subgenomic approach.” See the announcement and the list of other winning proposals on the IAPT site...","categories": ["articles"],
+        "tags": ["Diego Morales-Briones","grants","IAPT"],
+        "url": "http://localhost:4000/articles/diego-IAPT/"
+      },{
+        "title": "Congratulations, Sarah!",
+        "excerpt":"Sarah in the field.Congratulations to Sarah Jacobs for her successful student grant proposals to the American Society of Plant Taxonomists! Sarah’s proposal, “Examining differentiation of species criteria in diversification: a comparison of two species complexes in Castilleja (Orobanchaceae)” was funded by the ASPT to support her ongoing fieldwork in western...","categories": ["articles"],
+        "tags": ["Sarah Jacobs","student grants","ASPT"],
+        "url": "http://localhost:4000/articles/Sarah-ASPT/"
+      },{
+        "title": "Comparative phylogeography of the PNW rainforests proposal funded!",
+        "excerpt":"This new funding is a collaborative project with Jack Sullivan and Bryan Carstens that is focused on the development of a predictive framework for the discovery of cryptic biodiversity that can be applied to entire ecosystems and uses the disjunct rainforests of the Pacific Northwest as a model system for...","categories": ["articles"],
+        "tags": ["NSF","DEB","Biodiversity Discovery & Analysis","grants"],
+        "url": "http://localhost:4000/articles/NSF-Cryptic-Diversity/"
+      },{
+        "title": "Successful NSF DDIG proposals! Congratulations, Sarah and Diego!",
+        "excerpt":"Tank Lab graduate students Sarah Jacobs and Diego Morales-Briones were both awarded Doctoral Dissertation Improvement Grants from the NSF Systematics and Biodiversity Sciences, Phylogenetic Systematics program. Sarah Jacobs: Investigating the main contributors to plant speciation in a rapidly diverging lineage in western North America Diego Morales-Briones: Adaptive radiation and hybridization:...","categories": ["articles"],
+        "tags": ["NSF","DDIG","Phylogenetic Systematics","Diego","Sarah","grants"],
+        "url": "http://localhost:4000/articles/NSF-DDIGs/"
+      },{
+        "title": "BMC Evolution paper published",
+        "excerpt":"Nürk et al. 2015. BMC Evolutionary Biology.Nicolai’s paper investigating the biogeography and diversification dynamics of Hypericum came out today. Along with diversification rate analyses in BAMM and MEDUSA, we used bayou to investigate rates of climatic niche evolution associated with biogeographic movements and shifts in global climate. Nürk, NM, S...","categories": ["articles"],
+        "tags": ["<i>Hypericum</i>","Hypericaceae","niche evolution","bayou","BAMM","biogeography","diversification","Simon Uribe-Convers","Nicolai Nürk"],
+        "url": "http://localhost:4000/articles/BMC_Evol-paper/"
+      },{
+        "title": "Polyploidy and diversification rates paper published in New Phytologist",
+        "excerpt":"Tank et al. 2015. New Phytologist.The Plant Radiations special issue of New Phytologist was published this week, including our new paper investigating the whole genome duplication radiation lag-time model. Our paper provides the first statistical evidence supporting the hypothesis, demonstrating a link between paleopolyploidy and increased rates of net diversification....","categories": ["articles"],
+        "tags": ["WGD radiation lag-time hypothesis","angiosperm diversification","polyploidy","Matt Pennell","Jon Eastman","Luke Harmon"],
+        "url": "http://localhost:4000/articles/NewPhytologist-paper/"
+      },{
+        "title": "Tank Lab at Botany, 2015",
+        "excerpt":"Tank Lab members were involved in nine different presentations (7 talks, 2 posters) at the Botany meetings this year! All of my graduate students gave talks, and we even had one NSF REU student (Alice Butler, Bucknell University) present a poster on her 2015 summer project. Check them out here...","categories": ["articles"],
+        "tags": ["Botany 2015","presentations"],
+        "url": "http://localhost:4000/articles/Botany2015/"
+      },{
+        "title": "Dispersification paper published.",
+        "excerpt":"Uribe-Convers and Tank. 2015. AJB.Simon Uribe-Convers’ paper showing an increased diversification rate associated with the movement of Bartsia into the Andes was published in the current issue of the American Journal of Botany, and was highlighted by the editor here. Uribe-Convers, S and DC Tank. 2015. Shifts in diversification rates...","categories": ["articles"],
+        "tags": ["dispersification","biogeography","Bartsia","Neobartsia","Orobanchaceae","AJB","Simon Uribe-Convers"],
+        "url": "http://localhost:4000/articles/AJB-paper/"
+      },{
+        "title": "New paper by Hannah Marx published in Diversity and Distributions",
+        "excerpt":"Marx et al. 2016. Diversity and Distributions.Hannah Marx’s paper investigating Darwin’s Naturalization Conundrum using the flora of the San Juan Islands in the Pacific Northwest as a model was published today in Diversity and Distributions. This paper builds on the impressive floristic studies of our collaborators at the University of...","categories": ["articles"],
+        "tags": ["Darwin's Naturalization Conundrum","invasive species","flora of the San Juan Islands","community phylogenetics","Diversity and Distributions","Hannah Marx"],
+        "url": "http://localhost:4000/articles/DivDist-paper/"
+      },{
+        "title": "Latest paper from Simon Uribe-Convers published in PLoS ONE.",
+        "excerpt":"Fig 1. Floral diversity of the South American species of Bartsia, i.e., the Neobartsia clade. Fig 2. Flowchart describing the method used in this study.A new paper from my lab detailing a high-throughput approach for phylogenetic dataset collection using targeted, microfluidic PCR and Illumina sequencing came out this week in...","categories": ["articles"],
+        "tags": ["<i>Bartsia</i>","<i>Neobartsia</i>","microfluidic PCR","Fluidigm","high-throughput sequencing","phylogenomics","Matt Settles","Simon Uribe-Convers"],
+        "url": "http://localhost:4000/articles/PLoSONE-paper/"
+      },{
+        "title": "2016 Advanced Field Botany course now accepting applications.",
+        "excerpt":"We are very pleased to announce our fourth annual Advanced Field Botany course at the University of Idaho McCall Field Campus, scheduled for July 3 - July 16, 2016! This 3-credit course is aimed at upper level undergraduate, early career graduate students, and professionals with a strong interest in enhancing...","categories": ["articles"],
+        "tags": ["Advanced Field Botany","McCall Field Campus","Stillinger Herbarium"],
+        "url": "http://localhost:4000/articles/AFB-2016/"
+      }]
