38b0b956cc3642c3e3bfba0fc65d9ac8088350ab
CAH <hamiltonlab@uidaho.edu>
2019-06-27 15:38:37 -0700

    update

--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -2,5 +2,5 @@ var store = [{
         "title": "The Hamilton Lab is up and running!",
         "excerpt":"\tThe Hamilton Lab, part of the new Arthropod Molecular Systematics Lab (with coPI Marek Borowiec), has started! ","categories": ["articles"],
         "tags": ["hamilton lab"],
-        "url": "http://localhost:4000/articles/hamilton_lab_live/"
+        "url": "http://thehamiltonlab.github.io/articles/hamilton_lab_live/"
       }]
